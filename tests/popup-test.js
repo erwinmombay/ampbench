1b6a83c8f69a7cba44012eaf2fedc1db17b85c43
erwinmombay <erwin.mombay@gmail.com>
2017-07-10 11:18:18 -0700

    erwin changes

--- tests/popup-test.js
+++ tests/popup-test.js
@@ -13,7 +13,6 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-const chrome = require('sinon-chrome/extensions');
 
 import {
   isSupported,
